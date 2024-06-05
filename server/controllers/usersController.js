81903c99ee7faa63e650ea99e146f93607fcee06
AJ <akjwhatsapps@gmail.com>
2024-06-05 23:04:26 +0530

    Avatars Set

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -30,7 +30,39 @@ module.exports.login = async (req,res,next) =>{
         const{username, password} = req.body;
         const user = await User.findOne({ username });
         console.log("login handle ",req.body);
+        if(!user)
+            return res.json({msg:"Invalid UserName Or Passwrod" , status:false})
+        const userPassword = await bcrypt.compare(password ,user.password );
+        if(!userPassword)
+            return res.json({msg:"Invalid UserName Or Password ", status:false});
+        delete user.password;
+        return res.json({status:true , user});
     }catch(ex){
         next(ex);
+    }
+}
+
+
+// The function then uses the User.findByIdAndUpdate() method to find a user by ID (userId) and update their data. It updates two fields: isAvatarImageSet and avatarImage. It sets isAvatarImageSet to true and assigns the provided avatarImage to the user's data. 
+// The option { new: true } ensures that the function returns the updated user data.
+module.exports.setAvatars = async(req,res,next) =>{
+    try{
+        const userId = req.params.id;
+        const avatarImage = req.body.image;
+        const userData = await User.findByIdAndUpdate(
+            userId,
+            {
+                isAvatarImageSet:true,
+                avatarImage,
+            },{
+                new:true
+            }
+        );
+        return res.json({
+            isSet:userData.isAvatarImageSet,
+            image:userData.avatarImage
+        });
+    }catch(ex){
+        next(ex)
     }
 }
